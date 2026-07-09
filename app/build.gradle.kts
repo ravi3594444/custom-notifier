1d87bc598d302101a89cd88af5dc14f8f3d150cb
ravi3594444 <ravi3594444@users.noreply.github.com>
2026-07-09 13:44:40 +0000

    Shorten applicationId to under 30 characters

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.customnotifier.nxytw"
+    applicationId = "com.aistudio.notifier.nxytw"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
